181e1a342f776010b4fd680ae61fcdf4c154f09f
Esben Jacob Sloth <aum.eris@gmail.com>
2019-02-17 21:10:54 +0700

    Impact effect resources

--- Game/Assets/Scripts/ArrayAnimator.cs
+++ Game/Assets/Scripts/ArrayAnimator.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrayAnimator : MonoBehaviour{   
+    
     private enum DelayTypes{RandomStart,RandomDelay,SerialStart,SerialDelay}
     [SerializeField][Tooltip("Select the type of delay for your animations. You can randomize them or cycle through them according to their index in your animation array. You can chose to delay the onset of animations or start them inside the timeline.")]
     private DelayTypes delayType = DelayTypes.SerialStart;
+
     public Animator[] animations;
 
     void Awake(){
@@ -24,7 +26,6 @@ public class ArrayAnimator : MonoBehaviour{
             animation.speed = BPM/(60f*animation.GetCurrentAnimatorClipInfo(0).Length);
         }
     }
-
     private void RandomStart(){
         foreach (var animation in animations){
             animation.Play(0,0,UnityEngine.Random.Range(0f,1f));
@@ -39,14 +40,14 @@ public class ArrayAnimator : MonoBehaviour{
     private void RandomDelay(){
         foreach (var animation in animations){
             animation.enabled = false;
-            StartCoroutine(DelayedEnable(animation,UnityEngine.Random.Range(0f,1f)));
+            StartCoroutine(DelayedEnable(animation,1f+UnityEngine.Random.Range(1f,2f)));
         }
     }
     private void  SerialDelay(){
         for (int i = 0; i < animations.Length; i++){
             Animator animation = animations[i];
             animation.enabled = false;
-            StartCoroutine(DelayedEnable(animation,(float)i/(float)animations.Length));
+            StartCoroutine(DelayedEnable(animation,(1f+(float)i)/(float)animations.Length));
         }
     }
     private IEnumerator DelayedEnable(Animator animation, float delay){
