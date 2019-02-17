04d9dcf94582fb2f2d06187535cbfade4d834d5b
Esben Jacob Sloth <aum.eris@gmail.com>
2019-02-18 04:05:01 +0700

    4 o'clock commit
    Reflection settings and material assigner tool window

--- Game/Assets/Editor/MaterialAssigner.cs
+++ Game/Assets/Editor/MaterialAssigner.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEditor;
 
-public class MaterialAssigner : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+class MaterialAssigner : EditorWindow {
+    [MenuItem("Tools/Ozego/MaterialAssigner")]
+    public static void  ShowWindow () {
+        EditorWindow.GetWindow(typeof(MaterialAssigner));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    string searchQuery = "Sphere";
+    public string[] Strings = { "Larry", "Curly", "Moe" };
+    Material[] materials;
+    
+    void OnGUI () {
+        GUILayout.Label ("Material Assigner", EditorStyles.boldLabel);
+        searchQuery = EditorGUILayout.TextField("Prefab name:", searchQuery);
         
+        // "target" can be any class derrived from ScriptableObject 
+         // (could be EditorWindow, MonoBehaviour, etc)
+         ScriptableObject target = this;
+         SerializedObject so = new SerializedObject(target);
+         SerializedProperty stringsProperty = so.FindProperty("Strings");
+ 
+         EditorGUILayout.PropertyField(stringsProperty, true); // True means show children
+         so.ApplyModifiedProperties(); // Remember to apply modified properties
+        
+    }
+    private static GameObject[] FindObjectsByName(string SearchQuery){
+        return (GameObject[])Resources.FindObjectsOfTypeAll<GameObject>().Where(o => o.name == SearchQuery);
     }
 }
