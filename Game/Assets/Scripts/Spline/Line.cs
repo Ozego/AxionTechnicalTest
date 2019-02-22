47d07e7acea27b6185e5ba4b72efebabb39372a6
Esben Jacob Sloth <esben@pocketplaylab.com>
2019-02-22 15:35:57 +0700

    Remove garbage code

--- Game/Assets/Scripts/Spline/Line.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//Simplified bezier system derived from tutorial by Catlike Coding.
-public class Line : MonoBehaviour{
-    public Vector3 p0,p1;
-}
