588122bdf75e8313f78b9891b0a4b899e5cd11cb
Esben Jacob Sloth <aum.eris@gmail.com>
2019-02-24 23:57:08 +0700

    Scene Cleanup

---
+++ Game/Assets/Scripts/FX/PostEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class PostEffect : MonoBehaviour{
+    public RenderTexture buffer;
+    void OnRenderImage(RenderTexture source, RenderTexture destination){
+        // Graph
+        // buffer = RenderTexture.GetTemporary(source.width,source.height,0,source.format);
+        // Graphics.Blit(source,buffer);
+        // Graphics.Blit(buffer,destination);
+    }
+}
